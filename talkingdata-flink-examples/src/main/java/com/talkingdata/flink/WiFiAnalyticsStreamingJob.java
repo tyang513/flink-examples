ba0044ed8cb2a8726a7c493b2c53d74d4cd04334
tyang513 <tyang513@hotmail.com>
2020-01-07 16:54:15 +0800

    提交

--- talkingdata-flink-examples/src/main/java/com/talkingdata/flink/WiFiAnalyticsStreamingJob.java
+++ talkingdata-flink-examples/src/main/java/com/talkingdata/flink/WiFiAnalyticsStreamingJob.java
@@ -1,17 +1,17 @@
 package com.talkingdata.flink;
 
 import com.talkingdata.flink.function.ParserFlatMapFunction;
-import com.talkingdata.flink.kafka.CubeSerializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URL;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -27,17 +27,18 @@ public class WiFiAnalyticsStreamingJob {
 
     public static void main(String[] args) throws Exception {
 
+        InputStream inputStream = WiFiAnalyticsStreamingJob.class.getClassLoader().getResource("application.properties").openStream();
+        ParameterTool parameter = ParameterTool.fromPropertiesFile(inputStream);
+
         // 创建本地的环境
         StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // 配置环境
         environment.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         environment.getConfig().setAutoWatermarkInterval(1000L);
-
-        ParameterTool argsParameter = ParameterTool.fromArgs(args);
-        String configPath = argsParameter.get("path");
-
-        ParameterTool parameter = ParameterTool.fromPropertiesFile(configPath + "/application.properties");
+        environment.enableCheckpointing(1000, CheckpointingMode.EXACTLY_ONCE);
+        String checkpointPath = parameter.get("wifi-analytics.flink.checkpoint.path", "file:///tmp/flink/checkpoint");
+        environment.setStateBackend(new FsStateBackend(checkpointPath, true));
 
         String wiFiCollectorKafkaTopic = parameter.get("wifi-analytics.collector.kafka.topic", "wifi.collector");
         String wifiCubeKafkaTopic = parameter.get("wifi-analytics.cube.kafka.topic", "wifi.cube");
@@ -52,8 +53,11 @@ public class WiFiAnalyticsStreamingJob {
 
         FlinkKafkaConsumer consumer = new FlinkKafkaConsumer<>(wiFiCollectorKafkaTopic, new SimpleStringSchema(), kafkaProperties);
         consumer.setStartFromEarliest();
-        environment.addSource(consumer).flatMap(new ParserFlatMapFunction()).keyBy(0).print();
-                //.addSink(new FlinkKafkaProducer(wifiCubeKafkaTopic, new CubeSerializationSchema(), kafkaProperties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE));
+        environment.addSource(consumer).setParallelism(3)
+                .flatMap(new ParserFlatMapFunction())
+                .keyBy(0)
+                .print();
+        //.addSink(new FlinkKafkaProducer(wifiCubeKafkaTopic, new CubeSerializationSchema(), kafkaProperties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE));
 
         environment.execute("WiFi-Analytics-Steaming");
     }
